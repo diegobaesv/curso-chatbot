df52a856c2d495eed9de5f445c360ba4f8693048
DiegoB <diegobaesv@gmail.com>
2024-10-27 21:05:15 -0500

    avance  estados

--- src/core/bot-core.ts
+++ src/core/bot-core.ts
@@ -6,30 +6,79 @@ import { extractPhoneNumber, isContactMessage } from "../shared/util";
 export class BotCore {
 
     public static async procesarMensaje(client: Client, message: Message){
-        console.log('********************************************')
+        console.log('********************************************');
         if(isContactMessage(message.from)){
-            const TELEFONO = extractPhoneNumber(message.from);
-            const MENSAJE = message.body;
+            try {
+                const TELEFONO = extractPhoneNumber(message.from);
+                const MENSAJE = message.body;
 
-            console.log('telefono',TELEFONO);
-            console.log('mensaje',MENSAJE);
+                console.log('telefono',TELEFONO);
+                console.log('mensaje',MENSAJE);
 
-            const responseConversacionCabecera = await conversacionCabeceraService.obtenerUltimaConversacionCabecera(TELEFONO);
-            const conversacionCabecera = responseConversacionCabecera.data;
+                //OBTENGO STATUS ACTUAL DEL NUMERO QUE ME ESCRIBE
+                const responseConversacionCabecera = await conversacionCabeceraService.obtenerUltimaConversacionCabecera(TELEFONO);
+                const conversacionCabecera = responseConversacionCabecera.data;
 
-            const ESTADO = conversacionCabecera.ultimoEstado;
+                const ESTADO = conversacionCabecera.ultimoEstado;
+                const ID_CONVERSACION_CABECERA = conversacionCabecera.idConversacionCabecera;
 
-            const responseConversacion = await conversacionService.insertarConversacion({
-                autor: 'U',
-                idEstado: ESTADO,
-                idConversacionCabecera: conversacionCabecera.idConversacionCabecera,
-                mensaje: MENSAJE,
-                telefono: TELEFONO
-            });
+                //GUARDAR SU MENSAJE ENTRANTE CON SU ULTIMO ESTADO ENCONTRADO
+                await this.guardarMensajeUsuario(MENSAJE, ESTADO, ID_CONVERSACION_CABECERA, TELEFONO);
 
+                switch (ESTADO) {
+                    case 1:
+                        await this.enviarMensajeUsuario(
+                            `Hola, bienvenid@ al bot de Envios Devpool, estoy aquí para ayudarte. :)`
+                            ,client,2,ID_CONVERSACION_CABECERA,TELEFONO);
+
+                        await this.enviarMensajeUsuario(
+                            `Elije una de estas opciones:\n
+                            1. Programar dirección de envío\n
+                            2. Ver pedidos en curso\n
+                            3. Ver pedidos entregados\n
+                            4. Ver preguntas frecuentes`
+                            ,client,2,ID_CONVERSACION_CABECERA,TELEFONO);
+                        break;
+                    case 2:
+                        await this.enviarMensajeUsuario(
+                            `ahojra estas en el estado 2`
+                            ,client,3,ID_CONVERSACION_CABECERA,TELEFONO);
+                        break;
+                    case 3:
+                        await this.enviarMensajeUsuario(
+                            `ahojra estas en el estado 3`
+                            ,client,4,ID_CONVERSACION_CABECERA,TELEFONO);
+                        break;
+                    default:
+                        break;
+                }
+            } catch (error) {
+                console.error('Ha ocurrio un error general',error);
+            }
         } else {
             console.log(`Ignorando mensaje recibido de ${message.from}`);
         }
+    }
+
+    private static async guardarMensajeUsuario(mensaje: string, idEstado: number, idConversacionCabecera: number, telefono: string){
+        await conversacionService.insertarConversacion({
+            autor: 'U',
+            idEstado: idEstado,
+            idConversacionCabecera: idConversacionCabecera,
+            mensaje: mensaje,
+            telefono: telefono
+        });
+    }
+
+    private static async enviarMensajeUsuario(mensaje: string, client: Client, idEstado: number, idConversacionCabecera: number,  telefono: string){
+        await client.sendMessage(`${telefono}@c.us`, mensaje);
+        await conversacionService.insertarConversacion({
+            autor: 'U',
+            idEstado: idEstado,
+            idConversacionCabecera: idConversacionCabecera,
+            mensaje: mensaje,
+            telefono: telefono
+        });
     }
     
 }
